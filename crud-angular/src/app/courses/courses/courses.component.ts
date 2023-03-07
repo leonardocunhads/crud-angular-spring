3c08163f958f1608d575ace351a17d63e7d923fa
leonardocunhads <82096432+leonardocunhads@users.noreply.github.com>
2023-03-07 12:03:36 +0000

    commit

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-
+  courses: Course[] = [];
 }
